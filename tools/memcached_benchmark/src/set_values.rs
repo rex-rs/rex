a4c9a533a2439167d0b977aacdc1f9879bbf6fbf
Ruowen Qin <chinqrw@gmail.com>
2026-01-14 11:54:00 +0800

    memcached_benchmark: replace async-memcached with memcache-async
    Remove OpenSSL dependency by switching from async-memcached to
    memcache-async which uses only futures and tokio.
    
    Signed-off-by: Ruowen Qin <chinqrw@gmail.com>

--- tools/memcached_benchmark/src/set_values.rs
+++ tools/memcached_benchmark/src/set_values.rs
@@ -2,10 +2,12 @@ use std::collections::HashMap;
 use std::sync::Arc;
 
 use anyhow::Result;
-use async_memcached::AsciiProtocol;
 use log::info;
+use memcache_async::ascii::Protocol;
+use tokio::net::TcpStream;
 use tokio::sync::Semaphore;
 use tokio::task::JoinSet;
+use tokio_util::compat::TokioAsyncReadCompatExt;
 
 pub(super) async fn set_memcached_value(
     test_dict: Arc<HashMap<Arc<String>, Arc<String>>>,
@@ -13,14 +15,15 @@ pub(super) async fn set_memcached_value(
     port: String,
 ) -> Result<()> {
     info!("Start set memcached value");
-    let addr = format!("tcp://{}:{}", server_address, port);
+    let addr = format!("{}:{}", server_address, port);
     let mut sockets_pool = vec![];
     let concurrency_limit = 64;
 
     for _ in 0..concurrency_limit {
-        let client = async_memcached::Client::new(addr.as_str())
+        let stream = TcpStream::connect(&addr)
             .await
             .expect("TCP memcached connection failed");
+        let client = Protocol::new(stream.compat());
         sockets_pool.push(tokio::sync::Mutex::new(client));
     }
     let sockets_pool = Arc::new(sockets_pool);
@@ -37,16 +40,15 @@ pub(super) async fn set_memcached_value(
             let _permit = sem.acquire_owned().await;
             let mut socket = sockets_pool_clone[count & 0x3F].lock().await;
             socket
-                .set(&*key_clone, &*value_clone, None, None)
+                .set(&*key_clone, value_clone.as_bytes(), 0)
                 .await
                 .expect("memcached set command failed");
 
-            let ret = socket
+            let get_value = socket
                 .get(&*key_clone)
                 .await
                 .expect("memcached get command failed");
 
-            let get_value = ret.unwrap().data.unwrap();
             assert_eq!(get_value, value_clone.as_bytes());
         });
     }
