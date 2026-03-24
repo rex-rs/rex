8010fe76e4a713cf595d9e5d20f57596a3c7a3a8
Ruowen Qin <chinqrw@gmail.com>
2026-03-24 16:44:00 +0800

    rex/debug: remove unnecessary allow(improper_ctypes_definitions)
    - Warning no longer emitted as of Rust 1.94.0
    
    Signed-off-by: Ruowen Qin <chinqrw@gmail.com>

--- rex/src/debug.rs
+++ rex/src/debug.rs
@@ -3,7 +3,6 @@ use core::ffi::{c_int, c_uchar};
 use crate::ffi;
 
 // WARN must add "\0" at the end of &str, c_str is different from rust str
-#[allow(improper_ctypes_definitions)]
 #[unsafe(no_mangle)]
 pub(crate) unsafe extern "C" fn printk(fmt: &str, mut ap: ...) -> c_int {
     unsafe { ffi::vprintk(fmt.as_ptr() as *const c_uchar, ap) }
